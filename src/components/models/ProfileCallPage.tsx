ae29ebad59f45e83555e8f2e0fb905e06aab503d
anjanyelle <anjanyelle@gmail.com>
2024-10-05 11:51:53 +0530

    chnage

--- src/components/models/ProfileCallPage.tsx
+++ src/components/models/ProfileCallPage.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import Swal from 'sweetalert2'; // Import SweetAlert
 
 // Define types for EducationDetails and UserProfile
 interface EducationDetail {
@@ -15,7 +16,7 @@ interface UserProfile {
   lastName: string;
   email: string;
   mobileNumber: string;
-  dateOfBirth: string; // Changed to date for better handling
+  dateOfBirth: string;
   address: string;
   gender: string;
   city: string;
@@ -26,6 +27,30 @@ interface UserProfile {
   country: string;
   educationDetailsModelList: EducationDetail[];
 }
+
+// Reusable InputField Component
+const InputField = ({
+  name,
+  value,
+  placeholder,
+  type = 'text',
+  onChange,
+}: {
+  name: string;
+  value: string;
+  placeholder: string;
+  type?: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}) => (
+  <input
+    type={type}
+    name={name}
+    value={value}
+    onChange={onChange}
+    className="border border-gray-300 rounded-md p-2 text-sm text-black w-full focus:outline-none focus:border-indigo-500"
+    placeholder={placeholder}
+  />
+);
 
 const UserProfile = () => {
   const [userProfile, setUserProfile] = useState<UserProfile>({
@@ -53,18 +78,28 @@ const UserProfile = () => {
     ],
   });
 
-  const [isEditing, setIsEditing] = useState<boolean>(false);
-  const [isLoading, setIsLoading] = useState<boolean>(true); // New loading state
+  const [isLoading, setIsLoading] = useState<boolean>(true); // Loading state
 
   // Fetch user profile data
   useEffect(() => {
     const fetchUserProfile = async () => {
-      const usedId=localStorage.getItem("userId")
+      const userId = localStorage.getItem("userId");
+      if (!userId) {
+        console.error("User ID not found.");
+        return;
+      }
       try {
-        const response = await axios.get<UserProfile>(`https://meta.oxyloans.com/api/student-service/user/profile?id=${usedId}`);
+        const response = await axios.get<UserProfile>(
+          `https://meta.oxyloans.com/api/student-service/user/profile?id=${userId}`
+        );
         setUserProfile(response.data);
       } catch (error) {
         console.error('Error fetching user profile:', error);
+        Swal.fire({
+          icon: 'error',
+          title: 'Error',
+          text: 'Failed to fetch user profile.',
+        });
       } finally {
         setIsLoading(false); // Set loading to false once data is fetched
       }
@@ -72,151 +107,155 @@ const UserProfile = () => {
     fetchUserProfile();
   }, []);
 
+  // Validate inputs before form submission
+  const validateInputs = () => {
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(userProfile.email)) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Invalid Email',
+        text: 'Please enter a valid email address.',
+      });
+      return false;
+    }
+    if (!/^\d{10}$/.test(userProfile.mobileNumber)) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Invalid Mobile Number',
+        text: 'Mobile number should be 10 digits.',
+      });
+      return false;
+    }
+    if (!/^\d{6}$/.test(userProfile.pinCode)) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Invalid Pin Code',
+        text: 'Pin code should be 6 digits.',
+      });
+      return false;
+    }
+    return true;
+  };
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    if (!validateInputs()) return;
+
     try {
-      await axios.patch('https://meta.oxyloans.com/api/student-service/user/profile/update', userProfile);
-      alert('Profile updated successfully!'); // Show a success message
+      await axios.patch(
+        'https://meta.oxyloans.com/api/student-service/user/profile/update',
+        userProfile
+      );
+      Swal.fire({
+        icon: 'success',
+        title: 'Success',
+        text: 'Profile updated successfully!',
+      });
     } catch (error) {
       console.error('Error updating profile:', error);
-      alert('Failed to update profile. Please try again.'); // Show an error message
+      Swal.fire({
+        icon: 'error',
+        title: 'Update Failed',
+        text: 'Failed to update profile. Please try again.',
+      });
     }
   };
 
   if (isLoading) {
-    return <div>Loading...</div>; // Show loading state
+    return <div className="spinner">Loading...</div>; // Loading state (replace with a spinner or animation)
   }
 
   return (
     <div className="max-w-2xl mx-auto p-4">
-      <h2 className="text-xl font-semibold"  style={{color:'gray'}}>Profile Information</h2>
+      <h2 className="text-xl font-semibold" style={{ color: 'gray' }}>Profile Information</h2>
       <form onSubmit={handleSubmit}>
         <div className="grid grid-cols-2 gap-4 mt-4">
           {/* First Name and Last Name */}
-          <input
-            type="text"
+          <InputField
             name="firstName"
             value={userProfile.firstName}
-            onChange={(e) => setUserProfile({ ...userProfile, firstName: e.target.value })}
-            className="border border-gray-300 rounded-md p-2 text-sm text-black"
-         
             placeholder="First Name"
+            onChange={(e) => setUserProfile({ ...userProfile, firstName: e.target.value })}
           />
-          <input
-            type="text"
+          <InputField
             name="lastName"
             value={userProfile.lastName}
-            onChange={(e) => setUserProfile({ ...userProfile, lastName: e.target.value })}
-            className="border border-gray-300 rounded-md p-2 text-sm text-black"
-         
             placeholder="Last Name"
+            onChange={(e) => setUserProfile({ ...userProfile, lastName: e.target.value })}
           />
           {/* Email and Mobile Number */}
-          <input
-            type="email"
+          <InputField
             name="email"
+            type="email"
             value={userProfile.email}
-            onChange={(e) => setUserProfile({ ...userProfile, email: e.target.value })}
-            className="border border-gray-300 rounded-md p-2 text-sm text-black"
-         
             placeholder="Email"
+            onChange={(e) => setUserProfile({ ...userProfile, email: e.target.value })}
           />
-          <input
-            type="tel"
+          <InputField
             name="mobileNumber"
+            type="tel"
             value={userProfile.mobileNumber}
-            onChange={(e) => setUserProfile({ ...userProfile, mobileNumber: e.target.value })}
-            className="border border-gray-300 rounded-md p-2 text-sm text-black"
-         
             placeholder="Mobile Number"
+            onChange={(e) => setUserProfile({ ...userProfile, mobileNumber: e.target.value })}
           />
           {/* Date of Birth and Address */}
-          <input
-            type="date"
+          <InputField
             name="dateOfBirth"
+            type="date"
             value={userProfile.dateOfBirth}
+            placeholder="Date of Birth"
             onChange={(e) => setUserProfile({ ...userProfile, dateOfBirth: e.target.value })}
-            className="border border-gray-300 rounded-md p-2 text-sm text-black"
-         
           />
-          <input
-            type="text"
+          <InputField
             name="address"
             value={userProfile.address}
-            onChange={(e) => setUserProfile({ ...userProfile, address: e.target.value })}
-            className="border border-gray-300 rounded-md p-2 text-sm text-black"
-         
             placeholder="Address"
+            onChange={(e) => setUserProfile({ ...userProfile, address: e.target.value })}
           />
           {/* City and Pin Code */}
-          <input
-            type="text"
+          <InputField
             name="city"
             value={userProfile.city}
-            onChange={(e) => setUserProfile({ ...userProfile, city: e.target.value })}
-            className="border border-gray-300 rounded-md p-2 text-sm text-black"
-         
             placeholder="City"
+            onChange={(e) => setUserProfile({ ...userProfile, city: e.target.value })}
           />
-          <input
-            type="text"
+          <InputField
             name="pinCode"
             value={userProfile.pinCode}
-            onChange={(e) => setUserProfile({ ...userProfile, pinCode: e.target.value })}
-            className="border border-gray-300 rounded-md p-2 text-sm text-black"
-         
             placeholder="Pin Code"
+            onChange={(e) => setUserProfile({ ...userProfile, pinCode: e.target.value })}
           />
           {/* Organization and Designation */}
-          <input
-            type="text"
+          <InputField
             name="organization"
             value={userProfile.organization}
-            onChange={(e) => setUserProfile({ ...userProfile, organization: e.target.value })}
-            className="border border-gray-300 rounded-md p-2 text-sm text-black"
-         
             placeholder="Organization"
+            onChange={(e) => setUserProfile({ ...userProfile, organization: e.target.value })}
           />
-          <input
-            type="text"
+          <InputField
             name="designation"
             value={userProfile.designation}
-            onChange={(e) => setUserProfile({ ...userProfile, designation: e.target.value })}
-            className="border border-gray-300 rounded-md p-2 text-sm text-black"
-         
             placeholder="Designation"
+            onChange={(e) => setUserProfile({ ...userProfile, designation: e.target.value })}
           />
           {/* State and Country */}
-          <input
-            type="text"
+          <InputField
             name="state"
             value={userProfile.state}
-            onChange={(e) => setUserProfile({ ...userProfile, state: e.target.value })}
-            className="border border-gray-300 rounded-md p-2 text-sm text-black"
-         
             placeholder="State"
+            onChange={(e) => setUserProfile({ ...userProfile, state: e.target.value })}
           />
-          <input
-            type="text"
+          <InputField
             name="country"
             value={userProfile.country}
-            onChange={(e) => setUserProfile({ ...userProfile, country: e.target.value })}
-            className="border border-gray-300 rounded-md p-2 text-sm text-black"
-         
             placeholder="Country"
+            onChange={(e) => setUserProfile({ ...userProfile, country: e.target.value })}
           />
         </div>
 
-  
-        <div className="mt-4 flex justify-between">
-          {/* <button type="button" onClick={() => setIsEditing(!isEditing)} className="bg-blue-500 text-white py-2 px-4 rounded">
-            {isEditing ? 'Cancel' : 'Edit'}
-          </button> */}
-      
-            <button type="submit" className="bg-green-500 text-white py-2 px-4 rounded">
-              Save Changes
-            </button>
-   
+        <div className="mt-4 flex justify-end">
+          <button type="submit" className="bg-green-500 text-white py-2 px-4 rounded">
+            Save Changes
+          </button>
         </div>
       </form>
     </div>
