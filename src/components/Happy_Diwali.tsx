41d3444006fde5f8e08e0561a1f72c1d97b8963a
anjan kumar <anjanyelle@gmail.com>
2024-10-26 17:11:02 +0530

    ch

--- src/components/Happy_Diwali.tsx
+++ src/components/Happy_Diwali.tsx
@@ -27,6 +27,18 @@ const images = [
 const Happy_Diwali: React.FC = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
+  const handleNext = () => {
+    if (currentIndex < images.length - 1) {
+      setCurrentIndex(currentIndex + 1);
+    }
+  };
+
+  const handlePrev = () => {
+    if (currentIndex > 0) {
+      setCurrentIndex(currentIndex - 1);
+    }
+  };
+
   const goToPrevious = () => {
     const isFirstImage = currentIndex === 0;
     const newIndex = isFirstImage ? images.length - 1 : currentIndex - 1;
@@ -54,15 +66,22 @@ const Happy_Diwali: React.FC = () => {
   return (
     <>
       <Header1 />
-      <div className="event-container" style={{paddingBottom:'100px'}}>
+      <div className="event-container" style={{ paddingBottom: "100px" }}>
         <div className="event-content">
           <div className="diwali-images">
             <div className="image-container">
-              <img src={diyasImage} alt="Diwali Diyas" className="diwali-diya" />
+              <img
+                src={diyasImage}
+                alt="Diwali Diyas"
+                className="diwali-diya"
+              />
             </div>
           </div>
           <div className="event-details">
-            <h1 className="diwali-title" style={{ color: "rgba(121, 32, 199, 1)" }}>
+            <h1
+              className="diwali-title"
+              style={{ color: "rgba(121, 32, 199, 1)" }}
+            >
               Happy Diwali from Oxy Group!
             </h1>
             <h3 className="diwali-subtitle">
@@ -80,8 +99,8 @@ const Happy_Diwali: React.FC = () => {
             </p>
             <div className="buttons">
               <button onClick={handleWhatsAppClick} className="button demo">
-                <FontAwesomeIcon icon={faWhatsapp} className="whatsapp-icon" /> Send
-                WhatsApp Message
+                <FontAwesomeIcon icon={faWhatsapp} className="whatsapp-icon" />{" "}
+                Send WhatsApp Message
               </button>
             </div>
           </div>
@@ -107,10 +126,14 @@ const Happy_Diwali: React.FC = () => {
         </button>
       </div> */}
       <div>
-        <h1 style={{textAlign:'center',margin:"50px",fontSize:"50px"}}><b style={{color:'green'}}><span style={{color:'#0a6fba'}}>Oxy</span>Group</b> Companies</h1>
-       
-       
-        <div className="event-container1" >
+        <h1 style={{ textAlign: "center", margin: "50px", fontSize: "50px" }}>
+          <b style={{ color: "green" }}>
+            <span style={{ color: "#0a6fba" }}>Oxy</span>Group
+          </b>{" "}
+          Companies
+        </h1>
+
+        <div className="event-container1">
           <div className="event-content1">
             <div className="diwali-images1">
               <div className="image-container1">
@@ -124,144 +147,243 @@ const Happy_Diwali: React.FC = () => {
               <h1 className="diwali-title1" style={{ color: "#0a6fba" }}>
                 Lend & Earn 1.5% - 2.5% Monthly RoI
               </h1>
-              <h3 className="diwali-subtitle1" style={{padding:"0px",margin:'0px'}}>
-              OxyLoans.com is an RBI-approved P2P NBFC, a revolutionary fintech platform. We onboard tax-paying Individuals, and HNIs as Lenders. We enable lenders/ investors to exchange funds directly with borrowers. Our proprietary algorithms include credit scoring, underwriting, and loan agreement preparation.
+              <h3
+                className="diwali-subtitle1"
+                style={{ padding: "0px", margin: "0px" }}
+              >
+                OxyLoans.com is an RBI-approved P2P NBFC, a revolutionary
+                fintech platform. We onboard tax-paying Individuals, and HNIs as
+                Lenders. We enable lenders/ investors to exchange funds directly
+                with borrowers. Our proprietary algorithms include credit
+                scoring, underwriting, and loan agreement preparation.
               </h3>
-              <h3 className="diwali-subtitle1" style={{ fontWeight: 'bold' }}>
-                ₹1000000000+<b>DISBURSAL</b> <br></br>   30000+ <b>LENDERS</b><br></br>  270000+ <b>BORROWERS</b>
+              <h3 className="diwali-subtitle1" style={{ fontWeight: "bold" }}>
+                ₹1000000000+<b>DISBURSAL</b> <br></br> 30000+ <b>LENDERS</b>
+                <br></br> 270000+ <b>BORROWERS</b>
               </h3>
               <div className="buttons">
-              <a href="https://oxyloans.com/" target="_blank" rel="noopener noreferrer">
-  <button className="button demo">
-    Start Lending
-  </button>
-</a>
-
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="event-container1" >
-          <div className="event-content1" style={{borderColor:'#c26c27'}}>
-            <div className="diwali-images1">
-              <div className="image-container1">
-                <img src={img2} alt="Diwali Diyas" className="diwali-diya" />
-              </div>
-            </div>
-            <div className="event-details">
-            
-              <h1 className="diwali-title1" style={{ color: "#c26c27" }}>
-               Fractional Investments in Lands & Buildings
-              </h1>
-              <h3 className="diwali-subtitle1" style={{padding:"0px",margin:'0px',paddingBottom:"20px"}}>
-              OXYBRICKS is the first Blockchain platform that enables fractional investment in lands & buildings: a Blockchain tech platform that allows principal guarantee, monthly earnings, and property appreciation.
-              </h3>
-              
-              <div className="buttons">
-              <a href="https://oxybricks.world/" target="_blank" rel="noopener noreferrer">
-  <button className="button demo" style={{backgroundColor:"#c26c27"}}>
-  Know More
-  </button>
-</a>
-
+                <a
+                  href="https://oxyloans.com/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button className="button demo">Start Lending</button>
+                </a>
               </div>
             </div>
           </div>
         </div>
 
         <div className="event-container1">
-          <div className="event-content1" style={{ borderColor: '#05a446' }}>
+          <div className="event-content1" style={{ borderColor: "#c26c27" }}>
+            <div className="diwali-images1">
+              <div className="image-container1">
+                <img src={img2} alt="Diwali Diyas" className="diwali-diya" />
+              </div>
+            </div>
+            <div className="event-details">
+              <h1 className="diwali-title1" style={{ color: "#c26c27" }}>
+                Fractional Investments in Lands & Buildings
+              </h1>
+              <h3
+                className="diwali-subtitle1"
+                style={{ padding: "0px", margin: "0px", paddingBottom: "20px" }}
+              >
+                OXYBRICKS is the first Blockchain platform that enables
+                fractional investment in lands & buildings: a Blockchain tech
+                platform that allows principal guarantee, monthly earnings, and
+                property appreciation.
+              </h3>
+
+              <div className="buttons">
+                <a
+                  href="https://oxybricks.world/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button
+                    className="button demo"
+                    style={{ backgroundColor: "#c26c27" }}
+                  >
+                    Know More
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="event-container1">
+          <div className="event-content1" style={{ borderColor: "#05a446" }}>
             <div className="diwali-images1">
               <div className="image-container1">
                 <img src={img6} alt="Diwali Diyas" className="diwali-diya" />
               </div>
             </div>
             <div className="event-details">
-
               <h1 className="diwali-title1" style={{ color: "#05a446" }}>
                 Order . Rice . Online
               </h1>
-              <h3 className="diwali-subtitle1" style={{ padding: "0px", margin: '0px', paddingBottom: "20px",fontWeight:'bold' }}>
-               Free Delivery | All Over Hyderabad
+              <h3
+                className="diwali-subtitle1"
+                style={{
+                  padding: "0px",
+                  margin: "0px",
+                  paddingBottom: "20px",
+                  fontWeight: "bold",
+                }}
+              >
+                Free Delivery | All Over Hyderabad
               </h3>
-              <h3 className="diwali-subtitle1" style={{ padding: "0px", margin: '0px', paddingBottom: "20px", }}>
-              All type of rice brands available. Sri Lalitha, Kurnool, RRI, Cow brand, Sree Maateja,
-Kolam Rice, Surya Teja’s Brand, Gajraj Evergreen, Shubodayam, 5 Star, JSR
+              <h3
+                className="diwali-subtitle1"
+                style={{ padding: "0px", margin: "0px", paddingBottom: "20px" }}
+              >
+                All type of rice brands available. Sri Lalitha, Kurnool, RRI,
+                Cow brand, Sree Maateja, Kolam Rice, Surya Teja’s Brand, Gajraj
+                Evergreen, Shubodayam, 5 Star, JSR
               </h3>
-              <h3 className="diwali-subtitle1" style={{ padding: "0px", margin: '0px', paddingBottom: "20px",fontWeight:'bold' }}>
-              Return & Exchange Guarantee | Available Now : Steamed & Raw Rice
+              <h3
+                className="diwali-subtitle1"
+                style={{
+                  padding: "0px",
+                  margin: "0px",
+                  paddingBottom: "20px",
+                  fontWeight: "bold",
+                }}
+              >
+                Return & Exchange Guarantee | Available Now : Steamed & Raw Rice
               </h3>
 
-              <div className="buttons" >
-                <a href="https://erice.in/" target="_blank" rel="noopener noreferrer">
-                  <button className="button demo" style={{ backgroundColor: '#05a446' }}>
-                 order rice 
+              <div className="buttons">
+                <a
+                  href="https://erice.in/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button
+                    className="button demo"
+                    style={{ backgroundColor: "#05a446" }}
+                  >
+                    order rice
                   </button>
                 </a>
-
               </div>
             </div>
           </div>
         </div>
 
-        <div className="event-container1" >
-          <div className="event-content1" style={{borderColor:'#583e99'}}>
+        <div className="event-container1">
+          <div className="event-content1" style={{ borderColor: "#583e99" }}>
             <div className="diwali-images1">
               <div className="image-container1">
                 <img src={img4} alt="Diwali Diyas" className="diwali-diya" />
               </div>
             </div>
             <div className="event-details">
-            
               <h1 className="diwali-title1" style={{ color: "#583e99" }}>
-              All your ideas at one place
+                All your ideas at one place
               </h1>
-              <h3 className="diwali-subtitle1" style={{padding:"0px",margin:'0px',paddingBottom:"20px"}}>
-              BMV.Money is an Bridgital Marketplace connecting stakeholders in global immigration services, property management, machinery purchases, startup mentoring, and job orientation programs.
+              <h3
+                className="diwali-subtitle1"
+                style={{ padding: "0px", margin: "0px", paddingBottom: "20px" }}
+              >
+                BMV.Money is an Bridgital Marketplace connecting stakeholders in
+                global immigration services, property management, machinery
+                purchases, startup mentoring, and job orientation programs.
               </h3>
-              
-              <div className="buttons" >
-              <a href="https://bmv.money/" target="_blank" rel="noopener noreferrer">
-  <button className="button demo" style={{backgroundColor:'#583e99'}}>
-  Know More
-  </button>
-</a>
 
+              <div className="buttons">
+                <a
+                  href="https://bmv.money/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button
+                    className="button demo"
+                    style={{ backgroundColor: "#583e99" }}
+                  >
+                    Know More
+                  </button>
+                </a>
               </div>
             </div>
           </div>
         </div>
-        <div className="event-container1" >
-          <div className="event-content1" style={{ borderColor: '#189c9e' }}>
+        <div className="event-container1">
+          <div className="event-content1" style={{ borderColor: "#189c9e" }}>
             <div className="diwali-images1">
               <div className="image-container1">
                 <img src={img5} alt="Diwali Diyas" className="diwali-diya" />
               </div>
             </div>
             <div className="event-details">
-
               <h1 className="diwali-title1" style={{ color: "#189c9e" }}>
                 Find your dream home
               </h1>
-              <h3 className="diwali-subtitle1" style={{ padding: "0px", margin: '0px', paddingBottom: "20px" }}>
-                XPERT HOMES is a leading property management company offering transparent, high-quality services. We help property owners maximize ROI and find verified tenants through our comprehensive 360° management solutions.
+              <h3
+                className="diwali-subtitle1"
+                style={{ padding: "0px", margin: "0px", paddingBottom: "20px" }}
+              >
+                XPERT HOMES is a leading property management company offering
+                transparent, high-quality services. We help property owners
+                maximize ROI and find verified tenants through our comprehensive
+                360° management solutions.
               </h3>
 
-              <div className="buttons" >
-                <a href="https://xperthomes.in/" target="_blank" rel="noopener noreferrer">
-                  <button className="button demo" style={{ backgroundColor: '#189c9e' }}>
+              <div className="buttons">
+                <a
+                  href="https://xperthomes.in/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  <button
+                    className="button demo"
+                    style={{ backgroundColor: "#189c9e" }}
+                  >
                     Know More
                   </button>
                 </a>
-
               </div>
             </div>
           </div>
         </div>
-      
+
+        {/* Group Section */}
+        <div className="px-6 py-5 bg-[#f1f1f1] md:p-10">
+          <h2 className="mb-4 text-2xl font-bold text-center text-purple-700 md:text-3xl">
+            OxyGroup Companies
+          </h2>
+          <div className="relative w-full max-w-[800px] mx-auto overflow-hidden">
+            <button
+              className="absolute z-10 p-2 text-2xl transform -translate-y-1/2 bg-white rounded-full left-2 top-1/2"
+              onClick={handlePrev}
+            >
+              ←
+            </button>
+            <div
+              className="flex transition-transform duration-300 ease-in-out"
+              style={{ transform: `translateX(-${currentIndex * 100}%)` }}
+            >
+              {images.map((image, idx) => (
+                <div key={idx} className="flex-shrink-0 w-full">
+                  <img
+                    src={image.src}
+                    alt={image.alt}
+                    className="w-full h-auto"
+                  />
+                </div>
+              ))}
+            </div>
+            <button
+              className="absolute z-10 p-2 text-2xl transform -translate-y-1/2 bg-white rounded-full right-2 top-1/2"
+              onClick={handleNext}
+            >
+              →
+            </button>
+          </div>
+        </div>
       </div>
-   
 
       {/* <Firecracker /> */}
       <Footer />
